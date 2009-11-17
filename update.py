6882991899d92b3d7840affe44093e8cf84c13e5
Kalle Persson <kalle@nemus.se>
2009-11-17 17:13:56 +0100

    Added update script that pulls down the latest version from github

---
+++ update.py
@@ -0,0 +1,18 @@
+import os,sys,urllib2,tarfile
+print "Getting update..."
+update = urllib2.urlopen('http://github.com/kallepersson/subterranean-ng/tarball/master')
+
+archive = open('patch.tar.gz','wb')
+archive.write(update.read())
+print "Extracting update..."
+archive.close()
+tarball = tarfile.open('patch.tar.gz','r:gz')
+members = tarball.getmembers()
+folder = members[0].name
+tarball.extractall()
+print os.getcwd()
+os.chdir(folder)
+files = os.listdir('.')
+for file in files:
+	os.renames(file,'../'+file)
+sys.exit()
