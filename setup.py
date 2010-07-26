f93d6af9be087085fffde4e995692bd2e4bd9a94
kallepersson <kalle@nemus.se>
2010-07-26 20:37:56 +0200

    Made the installer create an app.

--- setup.py
+++ setup.py
@@ -7,21 +7,25 @@ Usage:
 from distutils.core import setup
 import py2app
 
+
 NAME = 'Subterranean'
 VERSION = '0.1'
 
 plist = dict(
-    CFBundleIconFile='English.lproj/Subterranean.icns',
     CFBundleName=NAME,
     CFBundleShortVersionString=VERSION,
     CFBundleGetInfoString=' '.join([NAME, VERSION]),
     CFBundleExecutable=NAME,
     CFBundleIdentifier='org.subterranean',
+    CFBundleIconFile='gameicon',
 )
 
 setup(
-data_files=['English.lproj', 'Assets','Libraries','Resources'],
+data_files=['English.lproj','gameicon.icns','Assets','Libraries','Resources'],
     app=[
         dict(script="main.py", plist=plist),
     ],
 )
+
+
+print "DONE"
